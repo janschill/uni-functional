dd8780bea0a724b1279e6b68bc7f79486b33b1f9
Jan Schill <janschill@me.com>
2018-03-27 19:23:19 +0200

    Add case when there is more money than bonbons

--- aufgabe1/aufgabe3.hs
+++ aufgabe1/aufgabe3.hs
@@ -1,8 +1,10 @@
 -- This program calculates the number of bonbons you can buy with the money you enter as a parameter in `user 155`
 
 -- user function sets counter and bonbon price to prevent cheating
+funcUser::(Num a, Ord a)=>a->a
 funcUser money = funcBonbon 0 money 10
 
 -- iterates counter as long as money is more than bonbonPrice
-funcBonbon counter money bonbonPrice | money < bonbonPrice = counter
-  | otherwise = funcBonbon (counter+1) (money-bonbonPrice) (z+10)
+funcBonbon::(Num a, Ord a)=>a->a->a->a
+funcBonbon counter money bonbonPrice | (money < bonbonPrice) || (bonbonPrice > 100) = counter
+  | otherwise = funcBonbon (counter+1) (money-bonbonPrice) (bonbonPrice+10)
