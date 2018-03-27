f367a02c71f514d8cbd9789b1c5d438f6df84be2
Jan Schill <janschill@me.com>
2018-03-27 19:22:57 +0200

    Use correct data type hinting

--- aufgabe1/aufgabe2.hs
+++ aufgabe1/aufgabe2.hs
@@ -1,4 +1,7 @@
 -- calculates the cross total of entered number
+funcQuer::(Integral a, Ord a)=>a->a
 funcQuer x | x < 10 = x
   | otherwise = (funcMod10 x) + funcQuer(div x 10)
-  where funcMod10 x = mod x 10
+  where
+    funcMod10::(Integral a, Ord a)=>a->a
+    funcMod10 x = mod x 10
