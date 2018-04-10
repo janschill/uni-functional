3c279fdefc9ffb1b91c31c8d1ac42000c0441a5b
Jan Schill <janschill@me.com>
2018-04-10 17:08:57 +0200

    [TASK] Use proper iteration

--- aufgabe3/aufgabe1.hs
+++ aufgabe3/aufgabe1.hs
@@ -1,9 +1,27 @@
+-- Squaresum
 f_rek::Integer->Integer
 f_rek 0 = 0
-f_rek a = a * a + f_rek (a-1)
+f_rek n = n * n + f_rek (n-1)
 
-fib_rek::Integer->Integer
+f_iter::Int->Int
+f_iter n = iter n 0
+  where
+    iter::Int->Int->Int
+    iter 0 akk = akk
+    iter n akk = iter (n-1) (akk + n*n)
+
+-- Fibonacci
+fib_rek::Int->Integer
 fib_rek 0 = 0
 fib_rek 1 = 1
-fib_rek a | a > 1 = fib_rek(a-1) + fib_rek(a-2)
-  | otherwise = 0
+fib_rek n = fib_rek(n-1) + fib_rek(n-2)
+
+fib_iter::Integer->Integer
+fib_iter 0 = 0
+fib_iter 1 = 1
+fib_iter n = iter n 0 1
+  where
+    iter::Integer->Integer->Integer->Integer
+    iter 0 akk1 _ = akk1
+    iter 1 _ akk2 = akk2
+    iter n akk1 akk2 = iter (n-1) akk2 (akk1+akk2)
