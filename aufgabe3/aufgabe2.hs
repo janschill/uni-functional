1e77717a1603e8c4d6ff0f1ea08cbf33df59298d
Jan Schill <janschill@me.com>
2018-04-10 12:38:01 +0200

    [TASK] Pascal triangle with list function

--- aufgabe3/aufgabe2.hs
+++ aufgabe3/aufgabe2.hs
@@ -3,6 +3,10 @@ fPascal 0 0 = 1
 fPascal row col | row >= 0 && col >= 0 = fPascal (row - 1) (col - 1) + fPascal (row - 1) (col)
   | otherwise = 0
 
+fPascalRow::Integer->Integer->[Integer]
+fPascalRow _ 0 = [1]
+fPascalRow a b = [fPascal a b] ++ fPascalRow a (b-1)
+
 fDreieck::Integer->[[Integer]]
 fDreieck 0 = [[1]]
-fDreieck a = [[fPascal a a] ++ [fPascal (a - 1) (a - 1)]]
+fDreieck a = [fPascalRow a a] ++ fDreieck (a-1)
