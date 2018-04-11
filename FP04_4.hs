a7079851e46f345995f77a8b88eb79f47de1bbb2
Jan Schill <janschill@me.com>
2018-04-11 15:58:24 +0200

    [CLEANUP] Refactor

--- FP04_4.hs
+++ FP04_4.hs
@@ -2,23 +2,23 @@ module FP04_4 where
 
 import FP03_2
 
-fFak::Integer->Integer
-fFak n = iter n 1
+f_fak::Integer->Integer
+f_fak n = iter n 1
   where
     iter::Integer->Integer->Integer
     iter 0 akk = akk
     iter n akk = iter (n-1) (n*akk)
 
-fBinom::Integer->Integer->Integer
-fBinom n k = div (fFak n) ((fFak k) * (fFak (n-k)))
+f_binom::Integer->Integer->Integer
+f_binom n k = div (f_fak n) ((f_fak k) * (f_fak (n-k)))
 
-fBinomTriangle::Integer->[[Integer]]
-fBinomTriangle a = reverse (fDreieck a)
+f_binom_triangle::Integer->[[Integer]]
+f_binom_triangle a = reverse (f_triangle a)
   where
-    fDreieck::Integer->[[Integer]]
-    fDreieck 0 = [[1]]
-    fDreieck a = [fPascalRow a a] ++ fDreieck (a-1)
+    f_triangle::Integer->[[Integer]]
+    f_triangle 0 = [[1]]
+    f_triangle a = [f_pascal_row a a] ++ f_triangle (a-1)
       where
-        fPascalRow::Integer->Integer->[Integer]
-        fPascalRow _ 0 = [1]
-        fPascalRow a b = [fBinom a b] ++ fPascalRow a (b-1)
+        f_pascal_row::Integer->Integer->[Integer]
+        f_pascal_row _ 0 = [1]
+        f_pascal_row a b = [f_binom a b] ++ f_pascal_row a (b-1)
