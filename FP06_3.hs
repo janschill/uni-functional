a477dd0016f52b3ecfd02e61867e7b435848895b
Jan Schill <jan.schill@visuellverstehen.de>
2018-04-30 12:45:40 +0200

    Filter repetition

--- FP06_3.hs
+++ FP06_3.hs
@@ -1,4 +1,6 @@
 {- removes repetition in a list -}
 
-filterRepeatition::[a]->[a]
--- filterRepeatition
+intList = [1,2,3,3,3,4,6,6,7]
+
+filterRepeatition::(Eq a)=>[a]->[a]
+filterRepeatition (x:xs) = foldr (\x xs -> if not (elem x xs) then [x] ++ xs else xs) [] (x:xs)
