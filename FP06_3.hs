e6dab78f17857117b2343d64f9c56cd79cbe6a06
Jan Schill <janschill@me.com>
2018-05-02 18:57:29 +0200

    [CLEANUP] Filter repetetition

--- FP06_3.hs
+++ FP06_3.hs
@@ -1,6 +1,9 @@
+module FP06_3 where
+
 {- removes repetition in a list -}
 
 intList = [1,2,3,3,3,4,6,6,7]
 
-filterRepeatition::(Eq a)=>[a]->[a]
-filterRepeatition (x:xs) = foldr (\x xs -> if (notElem x xs) then [x] ++ xs else xs) [] (x:xs)
+filterRepetition::(Eq a)=>[a]->[a]
+filterRepetition (x:xs) = foldr (\x xs -> if (notElem x xs) then [x] ++ xs else xs) [] (x:xs)
+
