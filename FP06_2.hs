c4d175557ed9600a65d2297f389275e2d31af449
Jan Schill <janschill@me.com>
2018-05-08 16:35:24 +0200

    Primes with list comprehension

--- FP06_2.hs
+++ FP06_2.hs
@@ -1,32 +1,11 @@
 module FP06_2 where
 
-import Prelude hiding (foldr)
+f_not_multiple::Integer->Integer->Bool
+f_not_multiple multiple num = mod num multiple /= 0
 
-intList = [1..10]
-intList2 = [[1..3],[4..7],[8..9]]
-
-{- use foldr to construct following functions -}
-filter'::(a->Bool)->[a]->[a]
-filter' _ [] = []
-filter' f (x:xs) = foldr (\x xs -> if f x then [x] ++ xs else xs ) [] (x:xs)
-
-map'::(a->b)->[a]->[b]
-map' _ [] = []
-map' f (x:xs) = foldr (\x xs -> [f x] ++ xs) [] (x:xs)
-
-concat'::[[a]]->[a]
-concat' [] = []
-concat' (x:xs) = foldr (\x xs -> x ++ xs) [] (x:xs)
-
-{- implementation help
-takes a function, a neutral element and a list
-applies the function to the first element with its rest recursively
-
-eg.:
-foldr (+) 0 [1..5]
-> 15
--}
-foldr::(a->b->b)->b->[a]->b
-foldr _ element [] = element
-foldr fun element (x:xs) = fun x (foldr fun element xs)
-
+f_prime_list::Integer->[Integer]
+f_prime_list n = f_sieve [2..n]
+  where
+    f_sieve::[Integer]->[Integer]
+    f_sieve [] = []
+    f_sieve (x:xs) = x : f_sieve [y | y <- xs, f_not_multiple x y]
