3ad21b1201e5592ba3af7cb4f29fb1e0ac57436d
Jan Schill <janschill@me.com>
2018-05-08 21:11:06 +0200

    [WIP] Pascal triangle iteratively

--- FP03_2.hs
+++ FP03_2.hs
@@ -16,7 +16,6 @@ f_triangle a = reverse (f_iter a)
         f_pascal_row _ 0 = [1]
         f_pascal_row a b = [f_pascal a b] ++ f_pascal_row a (b-1)
 
-
 -- fPascal::Int->Int->Integer
 -- fPascal _ 0 = 1
 -- fPascal row col = iter row col 0 0
