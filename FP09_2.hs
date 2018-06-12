1e4ab794b69cbb45046f20996728fad74e316d33
Jan Schill <janschill@me.com>
2018-06-12 15:54:10 +0200

    (Binary) Tree – equal, sum and show function implementation

--- FP09_2.hs
+++ FP09_2.hs
@@ -9,7 +9,27 @@ tree11 = Node' 1 [Node' 2 [], Node' 3 [], Node' 5 []]
 tree12 = Node' 2 [Node' 3 [], Node' 4 [], Node' 6 []]
 
 {-
-overwriting add function for binary tree
+implementation of (==) and (/=) for binary tree
+-}
+instance (Eq a) => Eq (BinaryTree a) where
+  (==) Empty Empty = True
+  (==) (Node left1 v1 right1) (Node left2 v2 right2) = (left1 == left2) && (v1 == v2) && (right1 == right2)
+
+  (/=) btree1 btree2 = not (btree1 == btree2)
+
+{-
+implementation of (==) and (/=) for tree
+-}
+instance (Eq a) => Eq (Tree a) where
+  (==) Empty' Empty' = True
+  (==) (Node' v1 trees1) (Node' v2 trees2) = (v1 == v2) && (aux trees1 trees2)
+    where
+      aux [] [] = True
+      aux (t1:ts1) (t2:ts2) = (t1 == t2) && (aux ts1 ts2)
+
+  (/=) tree1 tree2 = not (tree1 == tree2)
+{-
+implementation of (+) for binary tree
 -}
 instance (Num a) => Num (BinaryTree a) where
   (+) Empty Empty = Empty
@@ -17,8 +37,11 @@ instance (Num a) => Num (BinaryTree a) where
   (+) Empty btree = btree
   (+) (Node l1 v1 r1) (Node l2 v2 r2) = Node (l1 + l2) (v1 + v2) (r1 + r2)
 
+  (*) Empty Empty = Empty
+  (*) (Node l1 v1 r1) (Node l2 v2 r2) = Node (l1 * l2) (v1 * v2) (r1 * r2)
+
 {-
-overwriting add function for tree
+implementation of (+) for tree
 -}
 instance (Num a) => Num (Tree a) where
   (+) Empty' Empty' = Empty'
@@ -31,15 +54,20 @@ instance (Num a) => Num (Tree a) where
       sum [] trees = trees
       sum (t1:ts1) (t2:ts2) = (t1 + t2) : (sum ts1 ts2)
 
+  (*) Empty' Empty' = Empty'
+  (*) (Node' v1 trees1) (Node' v2 trees2) = Node' (v1*v2) (aux trees1 trees2)
+    where
+      aux [] [] = []
+      aux (t1:ts1) (t2:ts2) = (t1*t2):(aux ts1 ts2)
 {-
-overwriting show function for binary tree
+implementation of show function for binary tree
 -}
 instance (Show a) => Show (BinaryTree a) where
-  show Empty = ""
-  show (Node left value right) = show value ++ "\n" ++ "/" ++ "\\" ++ "\n" ++ show left ++ " " ++ show right ++ "\n"
+  show Empty = "Empty"
+  show (Node left value right) = show value ++ " " ++ show left ++ " " ++ show right
 
 {-
-overwriting show function for tree
+implementation of show function for tree
 -}
 instance (Show a) => Show (Tree a) where
   show Empty' = ""
