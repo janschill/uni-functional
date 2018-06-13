c1bbda6055dd7f9b3a8269c927b51c17b01bf1c2
Jan Schill <janschill@me.com>
2018-06-13 17:43:46 +0200

    Filter repetition in string

--- FP06_3.hs
+++ FP06_3.hs
@@ -5,5 +5,8 @@ module FP06_3 where
 intList = [1,2,3,3,3,4,6,6,7]
 
 filterRepetition::(Eq a)=>[a]->[a]
-filterRepetition (x:xs) = foldr (\x xs -> if (notElem x xs) then [x] ++ xs else xs) [] (x:xs)
+filterRepetition list = foldr (\x xs -> if (notElem x xs) then [x] ++ xs else xs) [] list
 
+filterRepetition'::(Eq a)=>[a]->[a]
+filterRepetition' [] = []
+filterRepetition' (x:xs) = x:(filterRepetition' (filter (/=x) xs))
